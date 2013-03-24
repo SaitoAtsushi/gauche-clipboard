7166d5ffbb9095f13e37382844d978231a13dcda
SaitoAtsushi <saito@s4.xrea.com>
2013-03-24 21:53:37 +0900

    Eliminated unneeded conversion of character encoding.

--- clipboard.scm
+++ clipboard.scm
@@ -11,10 +11,20 @@
 
 (dynamic-load "gauche_clipboard")
 
-(define (write-clipboard s)
-  (write-clipboard-internal
-   (ces-convert s (gauche-character-encoding) 'utf-8)))
+(define write-clipboard
+  (cond-expand
+   ((not gauche.ces.utf8)
+    (lambda(s)
+      (write-clipboard-internal
+       (ces-convert s (gauche-character-encoding) 'utf-8))))
+   (else write-clipboard-internal)))
 
-(define (read-clipboard)
-  (ces-convert (read-clipboard-internal) 'utf-8 (gauche-character-encoding)))
+(define read-clipboard
+  (cond-expand
+   ((not gauche.ces.utf8)
+    (lambda()
+      (ces-convert (read-clipboard-internal)
+                   'utf-8
+                   (gauche-character-encoding))))
+   (else read-clipboard-internal)))
 
